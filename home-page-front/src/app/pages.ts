52ac05ac64609f6f225db0675749421d28a276a3
Murilo - tqi_marantes <tqi_marantes@uolinc.com>
2019-05-28 19:23:20 -0300

    members page

--- home-page-front/src/app/pages.ts
+++ home-page-front/src/app/pages.ts
@@ -1,13 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
 
 export class Pages {
-    static readonly HOME_PATH = "home";
+    static readonly HOME_PATH = "home"
+    static readonly MEMBERS_PATH = "members"
 
     static readonly ROUTES: Routes = [
         {path: "", redirectTo: Pages.HOME_PATH, pathMatch: "full"},
-        {path:Pages.HOME_PATH, component: HomeComponent}
+        {path: Pages.HOME_PATH, component: HomeComponent},
+        {path: Pages.MEMBERS_PATH, component: MembersComponent}
     ];
     
-
+    static navigateToMembersPage(router: Router): void {
+        router.navigate([Pages.MEMBERS_PATH])
+    }
 }
